5d76e802d843b5a40cec1c9e66e123407582048e
chitsaou <chitsaou@techbang.com.tw>
2012-01-03 14:58:09 +0800

    set default SITE_NAME to the Rails application's class name

--- lib/seo_helper/engine.rb
+++ lib/seo_helper/engine.rb
@@ -5,5 +5,10 @@ module SeoHelper
     initializer "seo_helper.view_helpers" do
       ActionView::Base.send :include, SeoHelper::Helper
     end
+
+    initializer "seo_helper.default_site_name" do
+      # Set default SITE_NAME according to the Rails application class name
+      SITE_NAME = Rails.application.class.to_s.split("::").first if not defined? SITE_NAME
+    end
   end
 end
